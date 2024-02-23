6a8275c08d8425b85257bc7b046f1147806109c0
rohitrodge <webrohit27@gmail.com>
2024-02-23 23:18:47 +0530

    fetch Done

---
+++ fetch/fetch.js
@@ -0,0 +1,11 @@
+//fetch()
+
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
